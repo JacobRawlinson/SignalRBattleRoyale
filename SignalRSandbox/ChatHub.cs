7ad25056ed222ffeaa21e07a74cc52c4c1709346
Jacob Rawlinson <jacob.rawlinson@hotmail.co.uk>
2024-07-01 16:04:01 +0100

    Cleanup chat helper, add command to join game

--- SignalRSandbox/ChatHub.cs
+++ SignalRSandbox/ChatHub.cs
@@ -9,33 +9,28 @@ namespace SignalRSandbox
 {
     public class ChatHub : Hub
     {
-        private List<string> players = new List<string>()
-        {
-            "Matt",
-            "Leanne",
-            "Sarmon",
-            "Gideon",
-            "Claire",
-            "Will",
-            "Gaz",
-            "Pete",
-            "Sean",
-            "Kyle",
-            "Walshy",
-            "Andy H",
-            "Jacob",
-            "Chris",
-            "Stacy"
-        };
+        private List<string> players = new List<string>();
 
         public void Send(string name, string message)
         {
-            // Call the broadcastMessage method to update clients.
-
-            if (message == "StartMatch")
+            if (message.ToLower() == "/startmatch")
             {
                 GameHelper gh = new GameHelper();
                 gh.Start(this, players);
+                players = new List<string>();
+            }
+            else if (message.ToLower() == "/whois")
+            {
+                Broadcast($"Listing Players");
+
+                foreach (var player in players)
+                {
+                    Broadcast($"{player} is participating");
+                }
+            }
+            else if (message.ToLower() == "/join")
+            {
+                AddPlayer(name);
             }
             else
             {
@@ -46,6 +41,12 @@ namespace SignalRSandbox
         public void Broadcast(string message)
         {
             Clients.All.broadcastMessage(message);
+        }
+
+        private void AddPlayer(string name)
+        {
+            players.Add(name);
+            Broadcast($"{name} has joined the battle royale");
         }
 
         public void JoinChat(string name)
